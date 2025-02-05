8d17ea2d85c9e8b14aff9b43da06fab8961c1657
Ken Lin <kenlinahs@gmail.com>
2025-02-05 01:45:04 -0500

    Added route grouping

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router';
+import { Text, View } from 'react-native'
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+import "../global.css";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+
+  const [fontsLoaded, error] = useFonts({
+    "Kavoon_400Regular": require("../node_modules/@expo-google-fonts/kavoon/Kavoon_400Regular.ttf"),
+  })
+
+  useEffect(() =>{
+    if(error) throw error;
+
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if(!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false}}/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4E3F92",
+      },
+    },
   },
   plugins: [],
 };
