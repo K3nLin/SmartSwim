b675efe5a7cbda739e71c17caa7d1da24da82965
Ken Lin <KenLinAHS@gmail.com>
2025-02-07 03:47:44 -0500

    Swapped to png

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,31 +1,85 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { Tabs, Redirect } from 'expo-router';
-import HomeIcon from '../../assets/home-alt-svgrepo-com.svg';
+import HomeIcon from '../../assets/home.png';
+import HistoryIcon from '../../assets/history.png';
 import { SMALL_FONT, NORM_FONT, HEADER_FONT } from '../config.js';
+import * as cnfg from '../config.js';
+
+const TabIcon = ({icon, color, name, focused}) => {
+  return <View>
+    <Image
+      source={icon}
+      resizeMode="contain"
+      tintColor={color}
+      className={focused ? "w-7 h-7" : "w-6 h-6"}
+    />
+  </View>
+}
+
+const TabLabel = ({ name, focused }) => {
+  return (<Text
+      style={{
+        color: focused ? cnfg.TAB_ACTIVE_TINT : cnfg.TAB_INACTIVE_TINT,
+        fontSize: SMALL_FONT,
+        fontWeight: focused ? 'bold' : 'normal',
+      }}
+    >
+      {name}
+    </Text>
+  );
+};
 
 const TabsLayout = () => {
   return (
     
     <>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#FFFFFF',
+          tabBarStyle: {
+            backgroundColor: cnfg.TAB_BACKGROUND_COLOR
+          }
+        }}
+      >
           <Tabs.Screen
             name="home"
             options={{
               title: 'Home',
               headerShown: false,
               tabBarLabel: ({ focused }) => (
-                <Text
-                  style={{
-                    fontSize: focused ? NORM_FONT : SMALL_FONT,
-                    fontWeight: focused ? "bold" : "normal",
-                  }}
-                >
-                  Home
-                </Text>
+                <TabLabel
+                  name="Home"
+                  focused={focused}
+                />
               ),
-              tabBarIcon: ({ color, size, focused}) => (
-                <HomeIcon 
-                  width={focused ? size * 1.2 : size} height={focused ? size * 1.2 : size} fill={color} 
+              tabBarIcon: ({color, focused}) => (
+                <TabIcon
+                  icon={HomeIcon}
+                  color={color}
+                  name="Home"
+                  focused={focused}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="workout-history"
+            options={{
+              title: 'History',
+              headerShown: false,
+              tabBarLabel: ({ focused }) => (
+                <TabLabel
+                  name="History"
+                  focused={focused}
+                />
+              ),
+              tabBarIcon: ({color, focused}) => (
+                <TabIcon
+                  icon={HistoryIcon}
+                  color={color}
+                  name="History"
+                  focused={focused}
                 />
               ),
             }}
--- app/config.js
+++ app/config.js
@@ -2,3 +2,8 @@ export const SMALL_FONT = 12;
 export const NORM_FONT = 14;
 export const HEADER_FONT = 18;
 
+export const TAB_BACKGROUND_COLOR = "#332958";
+export const TAB_ACTIVE_TINT = "#FFA001";
+export const TAB_INACTIVE_TINT = "#FFFFFF";
+export const TAB_BUTTON_ACTIVE_COLOR = "#FFA001";
+export const TAB_BUTTON_INACTIVE_COLOR = "#332958";
