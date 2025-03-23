59bd0d5f203dcc3e864198897435a093241a4324
Ken Lin <KenLinAHS@gmail.com>
2025-03-22 21:53:01 -0400

    modified workoutSchema

---
+++ SmartSwim/backend/models/Workout.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const WorkoutSchema = new mongoose.Schema({
+  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  workoutDate: {type: Date, default: Date.now},
+  durationSeconds: {type: Number},
+  distance: {type: Number},
+  strokeCount: {type: Number},
+  seaState: {type: String},
+
+  rawData: [
+    {
+      Uptime: {type: Number},
+      Temperature: {type: Number},
+      acceleration: {type: [Number]},
+      position: {type: [Number]},
+    },
+  ],
+});
+
+module.exports = mongoose.model('Workout', WorkoutSchema);
